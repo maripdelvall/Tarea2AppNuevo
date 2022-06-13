f5963c95b269b0ade00f7d4849ec74226cc5a85a
MpD <mariapauladelvalle4@gmail.com>
2022-06-12 23:54:37 -0600

    Falta correr para ver el diseño

--- app/src/main/java/com/example/tarea2appnuevo/MainActivity.kt
+++ app/src/main/java/com/example/tarea2appnuevo/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.tarea2appnuevo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tarea2appnuevo.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //Initialize Firebase
+
+        FirebaseApp.initializeApp(this)
+        auth = Firebase.auth
+
+
+
     }
 }

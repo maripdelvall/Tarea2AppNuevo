31f3a98f1962050d16982aa8babd52be234f2fc4
MpD <mariapauladelvalle4@gmail.com>
2022-06-13 15:56:25 -0600

    Login and register error solved

--- app/src/main/java/com/example/tarea2appnuevo/MainActivity.kt
+++ app/src/main/java/com/example/tarea2appnuevo/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.tarea2appnuevo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.tarea2appnuevo.databinding.ActivityMainBinding
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
@@ -28,6 +30,52 @@ class MainActivity : AppCompatActivity() {
         auth = Firebase.auth
 
 
+        binding.login.setOnClickListener{
+            login()
+        }
 
+        binding.register.setOnClickListener{
+            register()
+        }
+
+
+
+    }
+
+
+    private fun login(){
+
+        val email = binding.editTextTextEmailAddress2.text.toString()
+        val password = binding.editTextTextPassword2.text.toString()
+
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this){ task ->
+            if (task.isSuccessful){
+                val user = auth.currentUser
+                Log.d("User Login", "Succes")
+            }else{
+                Log.d("User Login", "Failed")
+                Toast.makeText(baseContext, "Fail", Toast.LENGTH_LONG).show()
+
+            }
+
+        }
+
+    }
+
+    private fun register(){
+        val email = binding.editTextTextEmailAddress2.text.toString()
+        val password = binding.editTextTextPassword2.text.toString()
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this){ task ->
+            if (task.isSuccessful){
+                val user = auth.currentUser
+                Log.d("User Register", "Succes")
+            }else{
+                Log.d("User Register", "Failed")
+                Toast.makeText(baseContext, "Fail", Toast.LENGTH_LONG).show()
+
+            }
+
+        }
     }
 }
